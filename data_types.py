01ae26a37b1409df595e250d242f7455b24c7a09
hayatemraan <emraanhasanhayat@gmail.com>
2023-10-23 12:18:04 +0600

    updated

---
+++ data_types.py
@@ -0,0 +1,18 @@
+x = 23
+f = 23.0
+
+c = 1j + 2j
+
+y = "amr"
+
+z = False
+
+list_types = [1, 2, 3]
+
+tuple_types = (1, 2, 3)
+
+set_types = {1, 2, 3}
+
+dict_types = {"one": 1, "two": 2}
+
+no_value = None
---
+++ type_casting.py
@@ -0,0 +1,20 @@
+num = 12 
+
+num_str = str(num)
+
+print(num_str)
+
+num1 = "12"
+
+num2 = int(num1)
+
+print(num2)
+
+
+num2 = 24.0
+
+num3 = int(num2)
+
+print(num3)
+
+print(float(num))
